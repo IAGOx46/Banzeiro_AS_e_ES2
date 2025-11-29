723c2fec8fd24887c4edece6554f1e89dac38935
IAGOx46 <iagovinicius213@gmail.com>
2025-11-29 00:35:49 -0400

    att

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,6 @@ function App() {
 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
